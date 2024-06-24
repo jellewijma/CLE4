da5f43ed0f2e6aa6e446adf6e941a3959ba244a6
Jelle <jelle.wijma@gmail.com>
2024-06-24 10:05:38 +0200

    pwa

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default {
+    plugins: [
+        VitePWA({
+            // generates 'manifest.webmanifest' file on build
+            manifest: {
+                "name": "Barista Blitz",
+                "short_name": "Barista Blitz",
+                "icons": [
+                    {
+                        "src": "assets/logo.png",
+                        "sizes": "512x512",
+                        "type": "image/png"
+                    }
+                ],
+                "start_url": "https://jellewijma.github.io/CLE4/index.html",
+                "display": "fullscreen"
+            }
+        })
+    ]
+};
